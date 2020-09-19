17198fd6830995545221ebd4b228c93da65f21c6
ahmed-nossier <32619363+ahmed-nossier@users.noreply.github.com>
2020-09-19 17:52:39 +0300

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  images = [62, 83, 466, 965, 982, 1043, 738].map(
+    (n) => `https://picsum.photos/id/${n}/900/500`
+  );
+  trendingMovie: any[];
+  trendingtv: any[];
+  trendingtperson: any[];
+  imagesPrefixs: string = 'https://image.tmdb.org/t/p/w500';
+
   model: boolean = true;
-  constructor() {}
+  constructor(_moviesService: MoviesService) {
+    _moviesService.getTrendingMovie().subscribe((data) => {
+      this.trendingMovie = data.results;
+    });
+
+    _moviesService.getTrendingperson().subscribe((data) => {
+      this.trendingtperson = data.results;
+    });
+
+    _moviesService.getTrendingtv().subscribe((data) => {
+      this.trendingtv = data.results;
+    });
+  }
   modelX() {
-    this.model = !this.model;
+    this.model = false;
   }
   ngOnInit(): void {}
 }
